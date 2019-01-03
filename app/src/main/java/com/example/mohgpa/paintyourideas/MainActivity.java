8bb644f7d4503d7ac6d6764e5d4c16a03accfa26
mohitiitr <mkumar2@cs.iitr.ac.in>
2019-01-04 01:04:34 +0530

    final ui updates remaining ,its late night on jan 3,1:05 am ,2019,working for sds makers

--- app/src/main/java/com/example/mohgpa/paintyourideas/MainActivity.java
+++ app/src/main/java/com/example/mohgpa/paintyourideas/MainActivity.java
@@ -8,30 +8,42 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Spinner sizeSpinnerPen;
     Spinner colorSpinner;
     private MyCanvasView myCanvas;
+    Spinner eraserSpinner;
 
     private int penSize;
     private int eraserSize;
     private int currentColor;
 
+    private boolean isPenPressed=true;
+    private boolean isEraserPressed=false;
+
+    private int countBackPressed=0;
+    private boolean isOptionsVisible=false;
+
     String[] sizeNums={"1","2","3","4","5","6","7","8","9"};
     String [] colorsRange={ "Black","Red","Orange","SkyBlue","LightGreen","DarkGreen","Yellow","Pink","DarkBlue"};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Toast toast=Toast.makeText(MainActivity.this,"Please select PEN Button before changing pen attributes \n:)",Toast.LENGTH_SHORT);
+        final Toast toast2=Toast.makeText(MainActivity.this,"Please select ERASER Button before changing ERASER size \n:)",Toast.LENGTH_SHORT);
+
+
         myCanvas=(MyCanvasView) findViewById(R.id.canvastodraw);
 
 
         sizeSpinnerPen=(Spinner)findViewById(R.id.sizeSpinner);
         colorSpinner=(Spinner)findViewById(R.id.colorSpinner);
+        eraserSpinner=(Spinner)findViewById(R.id.sizeSpinnerForEraser);
 
         final ArrayAdapter penSizeAdapter=new ArrayAdapter(this, android.R.layout.simple_spinner_item, sizeNums);
         penSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -39,19 +51,48 @@ public class MainActivity extends AppCompatActivity {
 
 
         sizeSpinnerPen.setAdapter(penSizeAdapter);
+        eraserSpinner.setAdapter(penSizeAdapter);
         colorSpinner.setAdapter( new MyAdapter(MainActivity.this,R.layout.my_custom_layout,colorsRange));
 
         sizeSpinnerPen.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(isPenPressed){
                 penSize=position+1;
-                myCanvas.setPenSize((float)penSize*3);
+                myCanvas.setPenSize((float)penSize*4);
+            }
+            else {
+                     toast.show();
+                }
+
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                if (isPenPressed){penSize=16;
+                myCanvas.setPenSize((float)penSize);
+            }
+            else toast.show();
+            }
+        });
+
+        eraserSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              if(isEraserPressed){  eraserSize=position+1;
+                myCanvas.setPenSize((float)eraserSize*5);
+            }
+              else if (countBackPressed>0)toast2.show();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                penSize=4;
-                myCanvas.setPenSize(penSize);
+                if(isEraserPressed){eraserSize=16;
+                myCanvas.setPenSize((float)eraserSize);
+            }
+
             }
         });
 
@@ -59,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
         colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if(isPenPressed){
                 switch (position){
                     case 0:
                         currentColor=(int)R.color.cl4;
@@ -90,7 +133,9 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         currentColor=(int)R.color.cl4;
                 }
-                myCanvas.setPenColor(currentColor);
+                myCanvas.setPenColor(currentColor);}
+
+                else toast.show();
 
 
             }
@@ -101,6 +146,9 @@ public class MainActivity extends AppCompatActivity {
                 //myCanvas.setPenColor(currentColor);
             }
         });
+
+
+
     }
 
 
@@ -113,9 +161,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void setPen(View view){
         //setting the pen doing later
+        myCanvas.setPenSize(penSize*4);
+        myCanvas.setPenColor(currentColor);
+        isPenPressed=true;
+        isEraserPressed=false;
     }
 
     public void setEraser(View view){
         //setting eraser i will do later after the completion of spinner object
+        myCanvas.setPenSize(eraserSize*5);
+        myCanvas.setPenColor(R.color.White);
+        isPenPressed=false;
+        isEraserPressed=true;
+        countBackPressed++;
+    }
+
+    public void toolOptions(View view){
+
     }
 }
